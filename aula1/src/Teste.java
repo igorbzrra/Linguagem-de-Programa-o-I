18057f0221765e09718946ec03c55b21c90459cd
igorbzrra <20252PA-0100008@ifbapa.intra>
2026-09-22 17:43:01 -0300

    Primeiro Commit

---
+++ aula1/src/Teste.java
@@ -0,0 +1,10 @@
+public class Teste {
+    public static void main(String[] args) throws Exception {
+
+        int n = 7;
+        System.out.println(n >= 7 ? "Aprovado" : "reprovado" );
+
+        
+
+    }
+}
